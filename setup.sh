a83a76aaf01fba9c00415dc1d0bd9617e5448a5b
MariaMsu <i.knaz@yandex.ru>
2022-06-07 15:46:59 +0300

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
-mkdir ./add_lib
-cd ./add_lib
-
-git clone git@github.com:inspire-group/PatchGuard.git
-
-cd ./PatchGuard
-echo '' > __init__.py
-
-pip install git+https://github.com/wielandbrendel/bag-of-local-features-models.git
+mkdir ./add_lib && \
+  cd ./add_lib && \
+  git clone https://github.com/inspire-group/PatchGuard.git && \
+  cd ./PatchGuard && \
+  echo '' > __init__.py && \
+  pip install git+https://github.com/wielandbrendel/bag-of-local-features-models.git
